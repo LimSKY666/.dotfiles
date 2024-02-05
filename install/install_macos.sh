19b4d583bab4fb5749508cb412ea0c38b79949ce
Aleksey Mokrov <a.mokrov@tinkoff.ru>
2024-02-05 12:14:14 +0300

    Install brew as first step

--- install/install_macos.sh
+++ install/install_macos.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # brewtap
 brew tap Homebrew/bundle
 (cd ~/.dotfiles && brew bundle -v)
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 
 [ -f ~/.zshrc.pre-oh-my-zsh ] && cat ~/.zshrc.pre-oh-my-zsh >> ~/.zsh_local
 
